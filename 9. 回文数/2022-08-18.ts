e2a4aecd8c285fb33af2f3524f3471427e8228cb
lvqingxiang <1936684299@qq.com>
2022-08-20 13:42:03 +0800

    chore: ninth

---
+++ 9. 回文数/2022-08-18.ts
@@ -0,0 +1,25 @@
+function isPalindrome1(x: number): boolean {
+  let result = true;
+  if (x < 0) return false;
+  const str = x.toString();
+  const len = str.length;
+  let l = Math.floor(len / 2 - 1);
+  let r = len % 2 ? Math.floor(len / 2 + 1) : len / 2;
+  for (; r < len && l >= 0; l--, r++) {
+    if (str[l] !== str[r]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+function isPalindrome2(x: number): boolean {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+  let revertNumber = 0;
+  while (x > revertNumber) {
+    revertNumber = revertNumber * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  return x === revertNumber || x === Math.floor(revertNumber / 10);
+}
